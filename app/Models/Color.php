60b0327ec9f0eb41513f5b08e9bd35e56da83692
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2026-02-26 11:31:16 +0100

    Updated models for Flavor & Color - Added functions

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Models\Product;
 
 class Color extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
