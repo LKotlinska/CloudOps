f22ce7fb396c55d99b61436abcb2e2c9d708327a
Your Name <laura.kotlinska.96@gmail.com>
2026-03-02 15:10:32 +0100

    Refactor Category and Product models to include type hints for relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -20,4 +23,24 @@ class Product extends Model
         'category_id',
         'brand_id',
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function productVapes(): HasMany
+    {
+        return $this->hasMany(ProductVape::class);
+    }
+
+    public function flavors(): BelongsToMany
+    {
+        return $this->belongsToMany(Flavor::class);
+    }
 }
