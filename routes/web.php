7628cbe850553b478725c83e71090de9f64aae54
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2026-03-03 20:42:33 +0100

    Added routes for brands, flavors and colors

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\FlavorController;
+use App\Http\Controllers\ColorController;
+use App\Http\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/', fn() => redirect()->route('products.index'));
+
+Route::post('/login', LoginController::class);
+
+Route::post('/logout', LogoutController::class)->middleware('auth');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('products', ProductController::class);
+    Route::resource('brands', BrandController::class);
+    Route::resource('flavors', FlavorController::class);
+    Route::resource('colors', ColorController::class);
 });
