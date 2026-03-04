daa6e5320e711ae78528a5126836d50f875f6848
Your Name <laura.kotlinska.96@gmail.com>
2026-03-04 16:12:57 +0100

    Fixed anchor to use route and updated naming

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Categories</title>
 </head>
 <body>
     <form action="/logout" method="POST">
@@ -14,7 +14,7 @@
     
     <h1>Here are all the categories</h1>
 
-    <a href="./categories/create">New category</a>
+    <a href="{{ route('categories.create') }}">New category</a>
 
     @foreach ($categories as $category)
 
