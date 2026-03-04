f6570d5287453bb09397cc68f7d523101848ee18
Your Name <laura.kotlinska.96@gmail.com>
2026-03-04 16:55:49 +0100

    Renaming documents to represent its content

--- resources/views/categories/read.blade.php
+++ resources/views/categories/read.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ $category->name }}</title>
 </head>
 <body>
     <form action="/logout" method="POST">
@@ -12,7 +12,7 @@
         <button type="submit">Logout</button>
     </form>
     
-    <h1>Here are all the categories</h1>
+    <h1>Category view</h1>
 
     <a href="./categories/create">New category</a>
 
