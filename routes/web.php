c7c1628e4c15196d7e5ea178e719d5b1702ca6ce
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2026-03-04 21:06:22 +0100

    Added routes for start-page and categories

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,25 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\FlavorController;
 use App\Http\Controllers\ColorController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/', fn() => redirect()->route('products.index'));
+Route::get('/login', fn() => view('index'))->name('login');
 
 Route::post('/login', LoginController::class);
 
-Route::post('/logout', LogoutController::class)->middleware('auth');
+Route::post('/logout', LogoutController::class)->middleware('auth')->name('logout');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/start', [ProductController::class, 'startPage'])->name('start-page');
+
     Route::resource('products', ProductController::class);
     Route::resource('brands', BrandController::class);
     Route::resource('flavors', FlavorController::class);
     Route::resource('colors', ColorController::class);
+    Route::resource('categories', CategoryController::class);
 });
