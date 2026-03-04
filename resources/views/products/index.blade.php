b7df1c05d12c14bffa59b3a5fb12f74cf25ed051
Your Name <laura.kotlinska.96@gmail.com>
2026-03-04 16:56:09 +0100

    CRUD view for products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Products</title>
 </head>
 <body>
     <form action="/logout" method="POST">
@@ -13,8 +13,23 @@
     </form>
     
     <h1>Here are all the products</h1>
+
+    <a href="{{ route('products.create') }}">New product</a>
+
     @foreach ($products as $product)
-        <p>{{ $product->name }}</p>
+
+        <a href="{{ route('products.show', $product)}}">
+            {{ $product->name }}
+        </a>
+
+        <a href="{{ route('products.edit', $product) }}">Edit</a>
+
+        <form action="{{ route('products.destroy', $product)}}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Delete</button>
+        </form>
+
     @endforeach
 </body>
 </html>
