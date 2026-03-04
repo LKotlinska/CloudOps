80db1922a2c0b323107c53e39af3378bb2fb2944
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2026-03-04 11:27:57 +0100

    Updated js files, app.blade.php & web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\FlavorController;
+use App\Http\Controllers\ColorController;
 
+// Redirect root to products
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('products.index');
 });
+
+Route::resource('products', ProductController::class);
+Route::resource('brands',   BrandController::class);
+Route::resource('flavors',  FlavorController::class);
+Route::resource('colors',   ColorController::class);
