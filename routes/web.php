4ac248b9858b35f9b47c0552b84c0460e71de808
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2026-03-09 10:06:01 +0100

    Removed use App CategoryController duplicate

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FlavorController;
 use App\Http\Controllers\ColorController;
 use App\Http\Controllers\LogoutController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
