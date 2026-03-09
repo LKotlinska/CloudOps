343d442895e462dc8712bd6fe4e31f9b9f69cbad
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2026-03-09 20:01:15 +0100

    Added login.css and updated styling on login-page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&family=Syne:wght@400;600;700;800&display=swap"
+        rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/components.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <title>CloudOps</title>
 </head>
+
 <body>
     <main>
-        <h1>Login Page</h1>
-        @include('errors')
-        <form action="/login" method="POST">
-            @csrf
+        <div class="login">
+            <h1>Login Page</h1>
+            @include('errors')
+            <form action="/login" method="POST">
+                @csrf
 
-            <label for="email">Email address</label>
-            <input type="email" id="email" name="email" value="{{ old('email') }}">
+                <label for="email">Email address</label>
+                <input type="email" id="email" name="email" value="{{ old('email') }}">
 
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" value="{{ old('password') }}">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" value="{{ old('password') }}">
 
-            <button type="submit">Login</button>
+                <button type="submit" class="btn btn-primary">Login</button>
 
-        </form>
+            </form>
+        </div>
     </main>
 </body>
+
 </html>
