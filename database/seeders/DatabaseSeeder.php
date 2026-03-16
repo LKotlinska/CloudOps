d672408d02126fa8c1c861c2843b1b7622942761
Your Name <laura.kotlinska.96@gmail.com>
2026-03-16 17:16:16 +0100

    Change faker (dev dependency) to user::create (production) and password hash

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,10 +18,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' => '321'
+            'password' => Hash::make('321'),
         ]);
 
         $this->call([
